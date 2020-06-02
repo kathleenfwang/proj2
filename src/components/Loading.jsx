36b69349a06a0c944f399c46df0fdd388dabd3a2
kathleenfwang <kathleen.f.wang@gmail.com>
2020-06-01 20:28:33 -0400

    loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+export default function Loading() {
+  return (
+    <div class ="loading">
+      <h1> Loading </h1>
+      <img src ="https://d3toi7c4ip49i3.cloudfront.net/img/icon/loader.gif"/>
+    </div>
+  )
+}
