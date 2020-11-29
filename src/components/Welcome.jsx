0166f7ae2b1da0035c24d2d9a6481dc93384b22f
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-11-29 12:39:39 -0800

    take out landing home page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 
 export default function Welcome() {
-  window.scrollTo(0, 0);
-  return (
+  return (<div>
+  <Redirect to="/champions/Teemo" />}
+  {/* // window.scrollTo(0, 0);
+  // return (
    
-    <div className ="welcome">
-      <h1 > Welcome to Teemo's Adventure </h1>
+  //   <div className ="welcome">
+  //     <h1 > Welcome to Teemo's Adventure </h1>
 
-      <div className="welcomeMsg">
-          <p> Explore the different regions in League of Legends as Teemo and find out more information about the champions </p>
-        </div>
-      <Link to={{
-        pathname: "champions/Teemo",
-        state: {
-          name:'Teemo'
-        }
-      }}>
+  //     <div className="welcomeMsg">
+  //         <p> Explore the different regions in League of Legends as Teemo and find out more information about the champions </p>
+  //       </div>
+  //     <Link to={{
+  //       pathname: "champions/Teemo",
+  //       state: {
+  //         name:'Teemo'
+  //       }
+  //     }}>
         
-        <button>Start</button>
-      </Link> 
-    </div>
+  //       <button>Start</button>
+  //     </Link>  */}
+  </div>
   )
 }
