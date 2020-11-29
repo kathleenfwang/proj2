a5fcaa1030988a62b4b975ea823c887132cb5cad
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-11-29 12:55:09 -0800

    css hover

---
+++ build/precache-manifest.92ef8791aff36d279deb69e1bd17157d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e39eb9ccfa1ea55581c3aa3e5570d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "513b80ab9132cf4a0b7a",
+    "url": "/static/css/main.3bcfd86b.chunk.css"
+  },
+  {
+    "revision": "ba354d9317f0436d87a2",
+    "url": "/static/js/2.3c07f6a1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3c07f6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513b80ab9132cf4a0b7a",
+    "url": "/static/js/main.02850d3b.chunk.js"
+  },
+  {
+    "revision": "d0d0ca0e0eaf32a1f1bb",
+    "url": "/static/js/runtime-main.8b04e818.js"
+  }
+]);
